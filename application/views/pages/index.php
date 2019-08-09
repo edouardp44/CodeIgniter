9fb7db342101f561272be1c39d1821947498083b
Edouard <edouard.p44@gmail.com>
2019-08-09 16:47:49 +0200

    add pagination

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,5 +1,20 @@
-<script type="text/javascript" src="assets/js/index.js"></script>
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<p id="result"></p>
 
+<script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+<script
+	src="https://code.jquery.com/jquery-3.4.1.js"
+	integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+	crossorigin="anonymous"></script>
+<script type="text/javascript" src="assets/js/index.js"></script>
+<div id="loop">
+<div id="result" class="list"></div>
+</div>
+<nav aria-label="Page navigation example">
+	<ul class="pagination">
+		<li class="page-item">
+			<a id="previous-page" class="page-link" href="javascript:void(0)" aria-label="Previous">
+				<span aria-hidden="true">&laquo;</span>
+				<span class="sr-only">Previous</span>
+			</a>
+		</li>
+	</ul>
+</nav>
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+'use strict';
 fetch('/index.php/Api/get')
 	.then((res) => {
 		return res.json()
@@ -7,15 +8,85 @@ fetch('/index.php/Api/get')
 		data.forEach((user) => {
 			const {id, title, budget,description, publication } = user
 			result +=
-				`<div id="myTable">
-                     <h5> ID: ${id} </h5>
-                         <ul class="w3-ul">
-                             <li>  Title : ${title}</li>
-                             <li> Budget : ${budget} </li>
-                             <li> Description : ${description} </li>
-                             <li> Publication : ${publication} </li>
-                         </ul>
-                      </div>`;
+				`
+					<ul class="list-group">
+						<li class="list-group-item"> ID: ${id} </li>
+						<li class="list-group-item">  Title : ${title}</li>
+						<li class="list-group-item"> Budget : ${budget} </li>
+						<li class="list-group-item"> Description : ${description} </li>
+						<li class="list-group-item "> Publication : ${publication} </li>
+					</ul>
+				`;
 			document.getElementById('result').innerHTML = result;
+		});
+
+		var numbOfItem = $("#loop .list-group").length;
+		var limitPerPage = 5;
+
+		$(" #loop .list-group:gt(" + (limitPerPage - 1) + ")").hide();
+
+		var totalPages = Math.round(numbOfItem / limitPerPage);
+
+		$('.pagination').append("<li class='current-page page-item active'><a class='page-link' href='javascript:void(0)'>"+ 1 +"</a></li>");
+
+		for (var i =2; i <= totalPages; i++) {
+			$('.pagination').append("<li class='current-page page-item'><a class='page-link' href='javascript:void(0)'>"+ i +"</a></li>")
+		}
+
+		$('.pagination').append("<li id='next-page' class='page-item'><a class='page-link' href='javascript:void(0)' aria-label='Next'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>");
+
+		$('.pagination li.current-page').on('click', function () {
+			if ($(this).hasClass('active')) {
+				return false;
+			} else {
+				var currentPages = $(this).index();
+				$('.pagination li').removeClass('active');
+				$(this).addClass('active');
+				$('#loop .list-group').hide()
+
+				var grandTotal = limitPerPage * currentPages;
+
+				for (var j = grandTotal - limitPerPage; j < grandTotal; j++) {
+					$("#loop .list-group:eq(" + j + ")").show();
+
+				}
+			}
+		});
+
+		$("#next-page").on('click', function() {
+			var currentPage = $(".pagination li.active").index();
+			if (currentPage === totalPages) {
+				return false;
+			} else {
+				currentPage++;
+				$('.pagination li').removeClass('active');
+				$('#loop .list-group').hide();
+
+				var grandTotal = limitPerPage * currentPage;
+
+				for (var j = grandTotal - limitPerPage; j < grandTotal; j++) {
+					$("#loop .list-group:eq(" + j + ")").show();
+				}
+				$(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active');
+			}
+		});
+
+		$("#previous-page").on("click", function() {
+			var currentPage = $(".pagination li.active").index();
+			if (currentPage === 1) {
+				return false;
+			} else {
+				currentPage--;
+				$(".pagination li").removeClass('active');
+				$("#loop .list-group").hide();
+
+				var grandTotal = limitPerPage * currentPage;
+
+				for (var j = grandTotal - limitPerPage; j < grandTotal; j++) {
+					$("#loop .list-group:eq(" + j + ")").show();
+				}
+
+				$(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active');
+			}
 		});
 	});
