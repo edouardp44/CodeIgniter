8f883bded55a2b564d7cf66084c069b9be7da844
Edouard <edouard.p44@gmail.com>
2019-08-09 16:55:27 +0200

    add img for readMe

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,13 +1,16 @@
-
 <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+
 <script
 	src="https://code.jquery.com/jquery-3.4.1.js"
 	integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
 	crossorigin="anonymous"></script>
+
 <script type="text/javascript" src="assets/js/index.js"></script>
+
 <div id="loop">
-<div id="result" class="list"></div>
+	<div id="result" class="list"></div>
 </div>
+
 <nav aria-label="Page navigation example">
 	<ul class="pagination">
 		<li class="page-item">
